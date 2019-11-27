737e29cc3ec605af927aac32d1dda61cd2727b16
yuizho <yuizho3@gmail.com>
2019-11-28 00:07:04 +0900

    add kotlin configurations

---
+++ src/test/kotlin/io/github/yuizho/springsandbox/SpringsandboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.yuizho.springsandbox
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringsandboxApplicationTests {
+    @Test
+    fun test() {
+        assertTrue(true)
+    }
+}
