1dfd994e76c4854196cb1773f968be2700f499b2
yuizho <yuizho3@gmail.com>
2019-12-06 00:21:44 +0900

    add sample of downloading zip file

--- src/main/kotlin/io/github/yuizho/springsandbox/controllers/FileDownloadController.kt
+++ src/main/kotlin/io/github/yuizho/springsandbox/controllers/FileDownloadController.kt
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
-
+import java.io.ByteArrayOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 
 @RestController
@@ -39,5 +41,33 @@ class FileDownloadController {
             add(HttpHeaders.CONTENT_TYPE, "${MediaType.APPLICATION_OCTET_STREAM_VALUE}; charset=utf-8;")
         }
         return ResponseEntity(csv, headers, HttpStatus.OK)
+    }
+
+    @GetMapping("/zip")
+    fun downloadZip(): ResponseEntity<ByteArray> {
+        val zipFileByteArray = ByteArrayOutputStream().use { byteArrayOutputStream ->
+            ZipOutputStream(byteArrayOutputStream).use { zipOutputStream ->
+                // 圧縮する１つ目のファイル
+                val entry1 = ZipEntry("test/file1.txt")
+                val file1 = "これはfile1です".toByteArray()
+                entry1.size = file1.toUByteArray().size.toLong()
+                zipOutputStream.putNextEntry(entry1)
+                zipOutputStream.write(file1)
+                // 圧縮する２つ目のファイル
+                val entry2 = ZipEntry("test/file2.txt")
+                val file2 = "これはfile2です".toByteArray()
+                entry2.size = file2.toUByteArray().size.toLong()
+                zipOutputStream.putNextEntry(entry2)
+                zipOutputStream.write(file2)
+            }
+            // ZipOutputStreamをクローズした後呼ぶ必要がある！！
+            // https://stackoverflow.com/questions/40927894/java-zipinputstream-to-zipoutputstream-leads-to-end-of-central-directory-signa
+            byteArrayOutputStream.toByteArray()
+        }
+        val headers = HttpHeaders().apply {
+            add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"test.zip\"")
+            add(HttpHeaders.CONTENT_TYPE, "application/zip;")
+        }
+        return ResponseEntity(zipFileByteArray, headers, HttpStatus.OK)
     }
 }
