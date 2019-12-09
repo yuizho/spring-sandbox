06174f9edeb76d20dc19bd49df5416c345d9e375
yuizho <yuizho3@gmail.com>
2019-12-10 00:06:03 +0900

    modify file upload / download controller

--- src/main/kotlin/io/github/yuizho/springsandbox/controllers/FileUploadController.kt
+++ src/main/kotlin/io/github/yuizho/springsandbox/controllers/FileUploadController.kt
@@ -1,4 +1,28 @@
 package io.github.yuizho.springsandbox.controllers
 
-class FileUploadController {
+import io.github.yuizho.springsandbox.FileProperties
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+
+@RestController
+@RequestMapping("api/file/upload")
+class FileUploadController(
+        val fileProperties: FileProperties
+) {
+    @GetMapping("/test/{fileName}")
+    fun upload(@PathVariable fileName: String): String {
+        val folderPath = FileSystems.getDefault().getPath(fileProperties.path)
+        Files.createDirectories(folderPath)
+        val filePath = folderPath.resolve(Path.of(fileName))
+        // https://www.ne.jp/asahi/hishidama/home/tech/java/files.html
+        Files.writeString(filePath, "content", StandardOpenOption.CREATE_NEW)
+
+        return Files.readString(filePath)
+    }
 }
